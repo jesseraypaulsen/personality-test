54fcd0504ae04f7834ed5a5abb2f9fabdbf21da5
Jesse Paulsen <jesseraypaulsen@gmail.com>
2022-07-17 15:27:00 -0500

    feat: tooltips in the Header bar

--- Header.js
+++ Header.js
@@ -3,23 +3,30 @@ import ListIcon from "@mui/icons-material/List";
 import Box from "@mui/material/Box";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
 
 export default function Header({ setOpen }) {
   return (
     <AppBar sx={{ position: "static" }}>
-      <Toolbar>
+      <Toolbar sx={{ justifyContent: "space-evenly" }}>
         <Box sx={{ border: "1px solid black" }}>
-          <ListIcon
-            fontSize="large"
-            onClick={() => setOpen(true)}
-            sx={{ cursor: "pointer" }}
-          />
-          <BarChartIcon
-            fontSize="large"
-            color={true ? "disabled" : "primary"}
-            sx={{ cursor: "pointer" }}
-          />
+          <Tooltip title="Inventory">
+            <ListIcon
+              fontSize="large"
+              onClick={() => setOpen(true)}
+              sx={{ cursor: "pointer" }}
+            />
+          </Tooltip>
+          <Tooltip title="Results">
+            <BarChartIcon
+              fontSize="large"
+              color={true ? "disabled" : "primary"}
+              sx={{ cursor: "pointer" }}
+            />
+          </Tooltip>
         </Box>
+        <Typography>O.C.E.A.N. Personality Test</Typography>
       </Toolbar>
     </AppBar>
   );
